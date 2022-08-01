971c0e822d5e79d613abcd816b8c37769b2813b1
alvinarkansas <alvinarkansas@gmail.com>
2022-08-01 09:24:17 +0700

    feat: fetched users in a table

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { Table, Input } from "antd";
+import axios from "axios";
+import dayjs from "dayjs";
+import "antd/dist/antd.css";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([]);
+  const [columns] = useState([
+    {
+      title: "Username",
+      dataIndex: ["login", "username"],
+      key: "username",
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      render: (name) => <p>{`${name.first} ${name.last}`}</p>,
+    },
+    { title: "Email", dataIndex: "email", key: "email" },
+    { title: "Gender", dataIndex: "gender", key: "gender" },
+    {
+      title: "Registered Date",
+      dataIndex: ["registered", "date"],
+      key: "date",
+      render: (date) => <p>{dayjs(date).format("DD-MM-YYYY HH:mm")}</p>,
+    },
+  ]);
+
+  const loadUsers = async () => {
+    const { data } = await axios.get("https://randomuser.me/api", {
+      params: {
+        page: 1,
+        pageSize: 10,
+        results: 35,
+      },
+    });
+    setUsers(data.results);
+  };
+
+  useEffect(() => {
+    loadUsers();
+  }, []);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div style={{ display: "flex", gap: 16, marginBottom: 40 }}>
+        <div>
+          <p style={{ marginBottom: 0 }}>Search</p>
+          <Input.Search />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Table columns={columns} dataSource={users} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
